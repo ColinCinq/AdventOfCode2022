5963330e61275b1a7d82837443d57242c7ce704b
Colin <colin.cinq@gmail.com>
2022-12-14 03:33:39 +0100

    jour 3

--- public/js/script.js
+++ public/js/script.js
@@ -1,57 +1,57 @@
 $(document).ready(function () {
-  var words = ["Calorie Counting", "Rock Paper Scissors", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa"];
+    var words = ["Calorie Counting", "Rock Paper Scissors", "Rucksack Reorganization", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa", "aaaaaa"];
 
-  var date = new Date();
-  var day = date.getDate();
-  var month = date.getMonth() + 1;
-  var timeDelay = 200;
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var timeDelay = 200;
 
-  //   day = 26; // uncomment to skip to 25
+    //   day = 26; // uncomment to skip to 25
 
-  // Only work in December
-  if (month === 12) {
-    // Loop through each calendar window
-    $("li").each(function (index) {
-      var adventwindow = index + 1;
-      var item = $(this);
+    // Only work in December
+    if (month === 12) {
+        // Loop through each calendar window
+        $("li").each(function (index) {
+            var adventwindow = index + 1;
+            var item = $(this);
 
-      // Open past windows
-      if (day !== adventwindow && adventwindow < day) {
-        window.setTimeout(function () {
-          item.children(".door").addClass("open");
-        }, timeDelay);
-      }
+            // Open past windows
+            if (day !== adventwindow && adventwindow < day) {
+                window.setTimeout(function () {
+                    item.children(".door").addClass("open");
+                }, timeDelay);
+            }
 
-      // timeout offset for past window opening animation
-      timeDelay += 100;
+            // timeout offset for past window opening animation
+            timeDelay += 100;
 
-      // Add words so far to message variable
-      if (adventwindow <= day) {
-        var word = words[index];
-        $(this).append(
-          '<div class="revealed"> <a href="/day' +
-            adventwindow +
-            '">' +
-            word +
-            "</a> </div>"
-        );
-      }
+            // Add words so far to message variable
+            if (adventwindow <= day) {
+                var word = words[index];
+                $(this).append(
+                    '<div class="revealed"> <a href="/day' +
+                    adventwindow +
+                    '">' +
+                    word +
+                    "</a> </div>"
+                );
+            }
 
-      // Add jiggle animation to current day window
-      if (adventwindow === day) {
-        $(this).addClass("current");
-        $(this).addClass("jiggle");
-      }
+            // Add jiggle animation to current day window
+            if (adventwindow === day) {
+                $(this).addClass("current");
+                $(this).addClass("jiggle");
+            }
 
-      // On clicking a window, toggle it open/closed and
-      // handle other things such as removing jiggle and 25th
-      $(this).on("click", function () {
-        if (adventwindow <= day) {
-          $(this).children(".door").toggleClass("open");
-        }
+            // On clicking a window, toggle it open/closed and
+            // handle other things such as removing jiggle and 25th
+            $(this).on("click", function () {
+                if (adventwindow <= day) {
+                    $(this).children(".door").toggleClass("open");
+                }
 
-        $(this).removeClass("jiggle");
-      });
-    });
-  }
+                $(this).removeClass("jiggle");
+            });
+        });
+    }
 });
